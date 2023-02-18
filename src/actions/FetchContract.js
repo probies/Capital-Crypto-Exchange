dfd988d3b4a0a32ac066a6e67a5869347ff3c6e7
Piyush Jha <jhapiyu06@gmail.com>
2023-02-19 04:46:47 +0530

    Connect With Custom Backend

--- src/actions/FetchContract.js
+++ src/actions/FetchContract.js
@@ -4,23 +4,7 @@ export const FetchContract = async (after) => {
   console.log(after);
   try {
     const response = await axios.get(
-      `https://api.delta.exchange/v2/products?page_size=15${after ? `&after=${after}` : ""}`,
-      {
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          {
-            key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
-        ],
-      }
-    );
+      `https://capital-crypto-exchange-api.cyclic.app/api/products${after ? `&after=${after}` : ""}`);
     return await response.data;
   } catch (err) {
     return console.log(err);
