1eb4c67ce9ec11295a28ffdf287aec8b26c88193
Piyush Jha <jhapiyu06@gmail.com>
2023-02-19 01:57:50 +0530

    Fixed cors error

--- src/actions/FetchContract.js
+++ src/actions/FetchContract.js
@@ -4,10 +4,9 @@ export const FetchContract = async (after) => {
     try {
         const response = await fetch(`https://api.delta.exchange/v2/products?page_size=15${after?`&after=${after}`:''}`, {
             method: 'GET',
-            mode:'no-cors',
             headers: {
                 Accept: 'application/json',
-                'Access-Control-Allow-Origin': 'https://api.delta.exchange',
+                'Access-Control-Allow-Origin': 'https://api.delta.exchange,*',
                 "Access-Control-Allow-Credentials": true,
                 'Access-Control-Allow-Methods':'GET,PATCH,POST,PUT,DELETE',
 
