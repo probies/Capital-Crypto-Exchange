e33f6b6f7fa7929c02583f2d86bf161f269e6794
Piyush Jha <jhapiyu06@gmail.com>
2023-02-19 02:26:09 +0530

    Updated Library

--- src/actions/FetchContract.js
+++ src/actions/FetchContract.js
@@ -1,11 +1,20 @@
 import axios from "axios";
 //function to fetch data from delta exchange API and return data
 export const FetchContract = async (after) => {
-    console.log(after);
-    try {
-        const response = await axios.get(`https://api.delta.exchange/v2/products?page_size=15${after?`&after=${after}`:''}`)
-        return await response.data;
-    } catch (err) {
-        return console.log(err);
-    }
-}
+  console.log(after);
+  try {
+    const response = await axios.get(
+      `https://api.delta.exchange/v2/products?page_size=15${after ? `&after=${after}` : ""}`,
+      {
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Headers": "*",
+          "Access-Control-Allow-Credentials": "true",
+        },
+      }
+    );
+    return await response.data;
+  } catch (err) {
+    return console.log(err);
+  }
+};
